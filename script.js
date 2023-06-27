31ad61e78e84fda79207511829bb0bfbf6990932
mauriciosiachoque <jhonattanramos19@hotmail.com>
2023-06-26 20:54:32 -0500

    se realizo el clon de pinterest

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("hola")
+
+console.log("hola.log");
+
+document.write("Hola desde un write");
+
+
+
+//COdigo para consola, 
+
+//declaracion y asignación de variables
+
+//declaración  el = es la asignación  
+var nombre = prompt ("inserta tu nombre");
+
+console.log("el usuario es " + nombre )
